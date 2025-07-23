ff5b8d5691d9ed67b20d060429d1185b33022e12
Shubham <shubhamkharche2003@gmail.com>
2025-07-23 15:15:38 +0530

    GlobalExceptionHandler folder was created and handle all the exceptions

---
+++ src/main/java/com/shubham/Library/Management/System/Advice/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.shubham.Library.Management.System.Advice;
+
+import com.shubham.Library.Management.System.Exception.AuthorNotFoundException;
+import com.shubham.Library.Management.System.Exception.BookNotFoundException;
+import com.shubham.Library.Management.System.Exception.CategoryNotFoundException;
+import com.shubham.Library.Management.System.Exception.PublisherNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<String> handleGlobalException(BookNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(AuthorNotFoundException.class)
+    public ResponseEntity<String> handleGlobalException(AuthorNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(PublisherNotFoundException.class)
+    public ResponseEntity<String> handleGlobalException(PublisherNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(CategoryNotFoundException.class)
+    public ResponseEntity<String> handleGlobalException(CategoryNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGlobalException(Exception ex) {
+        return new ResponseEntity<>("Internal Server Error!", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
