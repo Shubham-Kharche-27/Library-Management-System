4fd6ee8eab535f496b11e5b539684f5626a1d96b
Shubham <shubhamkharche2003@gmail.com>
2025-07-23 19:31:37 +0530

    PublisherService and PublisherController was completed

--- src/main/java/com/shubham/Library/Management/System/Controller/PublisherController.java
+++ src/main/java/com/shubham/Library/Management/System/Controller/PublisherController.java
@@ -1,4 +1,46 @@
 package com.shubham.Library.Management.System.Controller;
 
+import com.shubham.Library.Management.System.Dto.PublisherDto;
+import com.shubham.Library.Management.System.Service.PublisherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/publisher")
 public class PublisherController {
+
+    @Autowired
+    private PublisherService publisherService;
+
+    @GetMapping("/get")
+    public ResponseEntity<Page<PublisherDto>> getAllPublishers(
+            @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+            @RequestParam(value = "pageSize", defaultValue = "3") int pageSize,
+            @RequestParam(defaultValue = "publisherId") String sortBy
+    ) {
+        return new ResponseEntity<>(publisherService.getAllPublisherData(pageNum - 1, pageSize, sortBy), HttpStatus.OK);
+    }
+
+    @GetMapping("/get/byId/{publisherId}")
+    public ResponseEntity<PublisherDto> getPublisherById(@PathVariable long publisherId) {
+        return new ResponseEntity<>(publisherService.getPublisherById(publisherId), HttpStatus.OK);
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<PublisherDto> createPublisher(@RequestBody PublisherDto publisherDto) {
+        return new ResponseEntity<>(publisherService.createPublisher(publisherDto), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/put/{publisherId}")
+    public ResponseEntity<String> updatePublisher(@PathVariable long publisherId, @RequestBody PublisherDto publisherDto) {
+        return new ResponseEntity<>(publisherService.updatePublisher(publisherId, publisherDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{publisherId}")
+    public ResponseEntity<String> deletePublisher(@PathVariable long publisherId) {
+        return new ResponseEntity<>(publisherService.deletePublisher(publisherId), HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/com/shubham/Library/Management/System/Service/PublisherService.java
+++ src/main/java/com/shubham/Library/Management/System/Service/PublisherService.java
@@ -1,4 +1,63 @@
 package com.shubham.Library.Management.System.Service;
 
+import com.shubham.Library.Management.System.Dto.PublisherDto;
+import com.shubham.Library.Management.System.Entity.Publisher;
+import com.shubham.Library.Management.System.Exception.PublisherNotFoundException;
+import com.shubham.Library.Management.System.Repository.PublisherRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PublisherService {
+
+    @Autowired
+    private PublisherRepo publisherRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Page<PublisherDto> getAllPublisherData(int pageNum, int pageSize, String sortBy) {
+        Pageable pageable = PageRequest.of(pageNum, pageSize, Sort.by(sortBy));
+        Page<Publisher> publisherPage = publisherRepo.findAll(pageable);
+        return publisherPage.map(Publisher -> modelMapper.map(Publisher, PublisherDto.class));
+    }
+
+    public PublisherDto getPublisherById(long publisherId) {
+        Publisher publisher = publisherRepo.findById(publisherId)
+                .orElseThrow(() -> new PublisherNotFoundException("Publisher does not exist!"));
+        return modelMapper.map(publisher, PublisherDto.class);
+    }
+
+    public PublisherDto createPublisher(PublisherDto publisherDto) {
+        Publisher publisher = modelMapper.map(publisherDto, Publisher.class);
+        return modelMapper.map(publisherRepo.save(publisher), PublisherDto.class);
+    }
+
+    public String updatePublisher(long publisherId, PublisherDto publisherDto) {
+        Publisher publisher = publisherRepo.findById(publisherId)
+                .orElseThrow(() -> new PublisherNotFoundException("Publisher does not exist!"));
+        if (publisherDto.getPublisherName() != null) {
+            publisher.setPublisherName(publisherDto.getPublisherName());
+        }
+        if (publisherDto.getPublisherAddress() != null) {
+            publisher.setPublisherAddress(publisherDto.getPublisherAddress());
+        }
+        if (publisherDto.getPublisherContactNum() != 0) {
+            publisher.setPublisherContactNum(publisherDto.getPublisherContactNum());
+        }
+        publisherRepo.save(publisher);
+        return "Publisher updated successfully!";
+    }
+
+    public String deletePublisher(long publisherId) {
+        Publisher publisher = publisherRepo.findById(publisherId)
+                .orElseThrow(() -> new PublisherNotFoundException("publisher does not exist!"));
+        publisherRepo.deleteById(publisherId);
+        return "Publisher deleted successfully!";
+    }
 }
